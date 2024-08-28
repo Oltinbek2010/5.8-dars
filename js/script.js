0a2280b270a693ad950f85f3e508adfddcbda8ab
Oltinbek2010 <oltinbek04@gmail.com>
2024-08-28 21:03:04 +0500

    commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// const body = document.getElementsByTagName('body');
+// const inputs = document.querySelectorAll('form.input');
+// const enter = document.getElementById('input');
+
+// body.style.height = '100vh';
+// body.style.padding = '100px'
+// body.style.background = 'linear-gradient(to top left, aqua, rgb(102, 197, 102), rgb(219, 230, 65))';
+// body.style.display = 'flex';
+// body.style.justifyContent = 'center';
+// body.style.alignItems = 'center';
+
+// inputs.style.width = '80px';
+// inputs.style.height = '80px';
+// inputs.style.borderRadius = '10px';
+// inputs.style.border = '20px', 'solid', 'rgb(0, 0, 0)';
+// inputs.style.background = 'rgb(129, 249, 201)';
+// inputs.style.color = 'rgb(0, 0, 0)';
+// inputs.style.fontSize = '36px';
+// inputs.style.margin = '5px';
+// inputs.style.alignItems = 'center';
+
+// enter.style.border = '5px', 'solid', 'rgb(0, 0, 0)';
+// enter.style.width = '360px';
